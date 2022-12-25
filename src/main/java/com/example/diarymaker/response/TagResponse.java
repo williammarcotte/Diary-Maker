efc0713efd5ad69598ec513913dcc0a6ff0d7a38
williammarcotte <billmarcotte125@gmail.com>
2022-12-24 19:03:17 -0500

    added getter and setter to tagresponse

--- src/main/java/com/example/diarymaker/response/TagResponse.java
+++ src/main/java/com/example/diarymaker/response/TagResponse.java
@@ -1,7 +1,15 @@
 package com.example.diarymaker.response;
 
 import com.example.diarymaker.entity.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TagResponse {
     private String text;
     private long id;
